8b9f253ec8cf7dc4f0afa20c4f19790f8a8b5f81
Brayan Esteves <brayan.esteves@insoftar.com>
2022-11-18 16:02:52 -0400

    ✅ Structure Base Api (Laravel) - CRUD [17.11.2022] ✅

---
+++ structure-base-api-laravel/app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Task;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $tasks = Task::all();
+        return response()->json($tasks, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        $task = Task::findOrFail($id);
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        $task = Task::findOrFail($id);
+        $task->delete($request->all());
+    }
+}
