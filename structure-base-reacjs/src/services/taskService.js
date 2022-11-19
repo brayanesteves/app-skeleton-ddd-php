e0d028cbd6a26d6c650d938d876d7e303bcfd390
Brayan Esteves <brayan.esteves@insoftar.com>
2022-11-19 12:31:30 -0400

    ✅ Structure Base (React.js) - Service (Task) [18.11.2022] ✅

---
+++ structure-base-reacjs/src/services/taskService.js
@@ -0,0 +1,42 @@
+const baseUrl = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/tasks`;
+
+export const loadTasks = () => {
+    return fetch(baseUrl).then((res) => res.json());
+};
+
+export const getTask = (id) => {
+    return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+};
+
+export const createTask = (task) => {
+    return fetch(baseUrl, {
+         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+           body: JSON.stringify({
+                title: task.name,
+            completed: task.completed
+        }),
+    }).then((res) => res.json());
+};
+
+export const updateTask = (task) => {
+    return fetch(`${baseUrl}/${task.id}`, {
+         method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+           body: JSON.stringify({
+                   id: task.id,
+                title: task.name,
+            completed: task.completed
+        }),
+    }).then((res) => res.json());
+};
+
+export const deleteTask = (id) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: "DELETE",
+    }).then(res => res.json());
+};
